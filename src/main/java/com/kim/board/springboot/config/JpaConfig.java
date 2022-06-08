6f3844711dd0446c28e5e1d0aeff6fb3b297caf7
kihyun <kkihyunzz@gmail.com>
2022-06-09 02:41:22 +0900

    spring security 사용

---
+++ src/main/java/com/kim/board/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kim.board.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
